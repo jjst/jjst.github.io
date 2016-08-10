25c687c2a1ec88e8f1b892ae732530f30afc7df7
Jeremie Jost <jeremiejost@gmail.com>
2016-08-10 15:30:06 -0400

    Use https for site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://jjst.github.io'
+SITEURL = 'https://jjst.github.io'
 RELATIVE_URLS = False
 
 FEED_DOMAIN = SITEURL
