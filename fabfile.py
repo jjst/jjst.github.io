a22b15cca02ac32fb450ee4024b240be6489aea7
Jeremie Jost <jeremiejost@gmail.com>
2017-06-25 15:02:39 +0100

    Upload/copy CNAME as part of gh_pages deploy

--- fabfile.py
+++ fabfile.py
@@ -91,5 +91,6 @@ def gh_pages():
     """Publish to GitHub Pages"""
     clean()
     local('pelican -s publishconf.py')
+    local("cp CNAME {deploy_path}".format(**env))
     local("ghp-import -b {github_pages_branch} {deploy_path}".format(**env))
     local("git push origin {github_pages_branch}".format(**env))
